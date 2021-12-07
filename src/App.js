9aacf870c2de87ac992ae40e5a1e7d01f34cbd7e
itallo gravina <itallo.gravina@gmail.com>
2021-12-07 17:37:31 -0300

    child-patter ok

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import FormikExample from "./forms"
+import React, { useState } from "react";
 
 function App() {
+  const [data, setData] = useState('');
+  
+  const childToParent = (childdata) => {
+    setData(childdata);
+  }
+  console.log(data)
   return (
     <div className="App">
-      <FormikExample/>
+      <FormikExample childToParent={childToParent}/>
     </div>
   );
 }
--- src/forms.js
+++ src/forms.js
@@ -4,20 +4,41 @@ import {
  FormControl,
  FormLabel,
  Input,
- InputGroup,
- InputRightAddon
+//  InputGroup,
+//  InputRightAddon
 } from '@chakra-ui/react'
 import React, { useState } from "react";
+// import useForm from './FormHook';
 
-export default function Stats(props) {
- const [emailAddress,setEmailAddress]= useState('')
- const [nomeAdress,setNomeAdres]= useState('')
- const[sobrenome,setSobrenome]=useState('')
+export default function Stats({childToParent}) {
+//  const [emailAddress,setEmailAddress]= useState('')
+//  const [nomeAdress,setNomeAdres]= useState('')
+//  const[sobrenome,setSobrenome]=useState('')
+ const initialFormData = Object.freeze({
+  email: "",
+  nome: "",
+  sobrenome:""
+});
+
+ const [fromData, setFromDat] = useState(initialFormData)
+
+const handleChange = (e)=>{
+
+  setFromDat({
+    ...fromData,
+    [e.target.name]:e.target.value.trim()
+
+  })
+}
+
  const handleSignin=(event)=>{
   event.preventDefault();
   console.log('i was subscript')
-  console.log(emailAddress,nomeAdress,sobrenome)
+  // console.log(emailAddress,nomeAdress,sobrenome)
+// console.log(fromData)
+
  }
+
  return (
   <form method="POST" onSubmit={handleSignin}>
 
@@ -25,20 +46,20 @@ export default function Stats(props) {
  
     <FormControl id='email'>
      <FormLabel>Email address</FormLabel>
-     <Input type='email' id="'email'" 
-     onChange={({target})=>setEmailAddress(target.value)}   
+     <Input name="email" type='email' id="'email'" 
+     onChange={handleChange}   
      />
      <FormLabel>Nome</FormLabel>
-     <Input type='nome' id='nome'
-      onChange={({target})=>setNomeAdres(target.value)}
+     <Input name="nome" id='nome'
+         onChange={handleChange}   
      />
      <FormLabel>Sobrenome</FormLabel>
-     <Input type='sobrenome' id='sobrenome'
-     onChange={({target})=>setSobrenome(target.value)}
+     <Input name="sobrenome"type='sobrenome' id='sobrenome'
+         onChange={handleChange}   
      />
     </FormControl>
     <FormControl>
-    <Button type='submit' colorScheme='blue' >Button</Button>
+    <Button type='submit' colorScheme='blue' onClick={()=>childToParent(fromData)} >Button</Button>
     </FormControl>
    </Stack>
   </form>
