4d8baf14e82f4c66fa50397fc1fc9f8a18b7854b
itallogravina <itallo.gravina@gmail.com>
2021-12-07 22:57:03 -0300

    object

--- src/App.js
+++ src/App.js
@@ -8,6 +8,33 @@ function App() {
     setData(childdata);
   }
   console.log(data)
+
+  const myObj = [
+{
+    'name': 'Umut',
+    'age' : 34
+},
+{
+  'name': 'Umut',
+  'age' : 34
+},
+{
+  'name': 'Umut',
+  'age' : 34
+}
+];
+
+const myObj2 = {
+  name: 'Umut',
+  age : 1
+}
+
+Object.entries(myObj).forEach(([key, value]) => {
+  value.name=myObj2.name+key
+  value.age=myObj2.age+key
+  console.log(value)
+});
+
   return (
     <div className="App">
       <FormikExample childToParent={childToParent}/>
