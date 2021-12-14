6e67ed216b2fa57a9031dffffe0c27b75e74c30f
itallo gravina <itallo.gravina@gmail.com>
2021-12-14 09:24:57 -0300

    Base

--- src/App.js
+++ src/App.js
@@ -1,43 +1,10 @@
-import FormikExample from "./forms"
-import React, { useState } from "react";
+
+import React from "react";
 
 function App() {
-  const [data, setData] = useState('');
-  
-  const childToParent = (childdata) => {
-    setData(childdata);
-  }
-  console.log(data)
-
-  const myObj = [
-{
-    'name': 'Umut',
-    'age' : 34
-},
-{
-  'name': 'Umut',
-  'age' : 34
-},
-{
-  'name': 'Umut',
-  'age' : 34
-}
-];
-
-const myObj2 = {
-  name: 'Umut',
-  age : 1
-}
-
-Object.entries(myObj).forEach(([key, value]) => {
-  value.name=myObj2.name+key
-  value.age=myObj2.age+key
-  console.log(value)
-});
 
   return (
     <div className="App">
-      <FormikExample childToParent={childToParent}/>
     </div>
   );
 }
